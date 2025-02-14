76f3e3e596689e9536ac96669ae254c250c58f92
Evandro-Prog <evandro.presenci@gmail.com>
2025-02-13 23:40:25 -0300

    Implementação chave JWT

--- DevagramCSharp/Controllers/LoginController.cs
+++ DevagramCSharp/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
 ﻿using DevagramCSharp.Dtos;
+using DevagramCSharp.Models;
+using DevagramCSharp.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
@@ -23,7 +25,46 @@ namespace DevagramCSharp.Controllers
         {
             try
             {
-                throw new ArgumentException("Erro ao preencher os dados");
+               if (!string.IsNullOrEmpty(loginrequisicao.Senha) && !string.IsNullOrEmpty(loginrequisicao.Email) &&
+                    !string.IsNullOrWhiteSpace(loginrequisicao.Senha) && !string.IsNullOrWhiteSpace(loginrequisicao.Email))
+                {
+                    string email = "evandro@evandro.com.br";
+                    string senha = "Senha1234@";
+
+                    if (loginrequisicao.Email == email && loginrequisicao.Senha == senha)
+                    {
+
+                        Usuario usuario = new Usuario()
+                        {
+                            Email = loginrequisicao.Email,
+                            Id = 12,
+                            Nome = "Evandro Marques"
+                        };
+
+                        return Ok(new LoginRespostaDto()
+                        {
+                            Email = usuario.Email = email,
+                            Nome = usuario.Nome,
+                            Token = TokenService.CriarToken(usuario)
+                        });
+                    }
+                    else
+                    {
+                        return BadRequest(new ErrorResponseDto()
+                        {
+                            Descricao = "Email ou senha inválido",
+                            Status = StatusCodes.Status400BadRequest
+                        });
+                    }
+                }
+                else
+                {
+                    return BadRequest(new ErrorResponseDto()
+                    {
+                        Descricao = "Usuario não preencheu os dados corretamente",
+                        Status = StatusCodes.Status400BadRequest
+                    });
+                }
             }
             catch (Exception ex)
             {
